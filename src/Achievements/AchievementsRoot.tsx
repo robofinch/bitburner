ea25b24ebb879e35f89657b1425b0e9336564b94
robofinch <86991566+robofinch@users.noreply.github.com>
2024-07-18 23:38:46 -0500

    ACHIEVEMENTS: Add description to achievement page
    Alignment now also matches Milestones page

--- src/Achievements/AchievementsRoot.tsx
+++ src/Achievements/AchievementsRoot.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import { Theme } from "@mui/material/styles";
 import { AchievementList } from "./AchievementList";
 import { achievements } from "./Achievements";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Player } from "@player";
 import { makeStyles } from "tss-react/mui";
 
 const useStyles = makeStyles()((theme: Theme) => ({
   root: {
     width: 50,
-    padding: theme.spacing(2),
     userSelect: "none",
   },
 }));
@@ -20,7 +19,13 @@ export function AchievementsRoot(): JSX.Element {
   return (
     <div className={classes.root} style={{ width: "90%" }}>
       <Typography variant="h4">Achievements</Typography>
-      <AchievementList achievements={Object.values(achievements)} playerAchievements={Player.achievements} />
+      <Box mx={2}>
+        <Typography>
+          Achievements are a persistent reward for completing challenges. A limited number of Bitburner achievements
+          have corresponding achievements in Steam.
+        </Typography>
+        <AchievementList achievements={Object.values(achievements)} playerAchievements={Player.achievements} />
+      </Box>
     </div>
   );
 }
